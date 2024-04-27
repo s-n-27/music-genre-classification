af64e6f0becc54161df6bec59805b4ebf614e344
s-n-27 <122672044+s-n-27@users.noreply.github.com>
2024-04-27 11:39:32 +0530

    Add files via upload

---
+++ audio_features.py
@@ -0,0 +1,102 @@
+#Audio Features
+import numpy as np
+import librosa, librosa.display
+import matplotlib.pyplot as plt
+
+FIG_SIZE = (10,8)
+
+file = "/kaggle/input/gtzan-genre-collection/genres/blues/blues.00000.au"
+
+# load audio file with Librosa
+signal, sample_rate = librosa.load(file, sr=22050)
+
+# signal stores amplitude for each of time frame.
+# 1s being split into 22050 parts
+# sample_rate = sr
+
+# WAVEFORM
+# display waveform
+plt.figure(figsize=FIG_SIZE)
+librosa.display.waveshow(y=signal, sr=sample_rate, alpha=0.4)
+plt.xlabel("Time (s)")
+plt.ylabel("Amplitude")
+plt.title("Waveform")
+
+
+# FFT -> power spectrum
+# perform Fourier transform
+fft = np.fft.fft(signal)
+# fft contains a complex value
+
+# calculate abs values on complex numbers to get magnitude
+spectrum = np.abs(fft)
+
+# create frequency variable
+f = np.linspace(0, sample_rate, len(spectrum))
+# plot spectrum
+plt.figure(figsize=FIG_SIZE)
+plt.plot(f, spectrum, alpha=0.4)
+plt.xlabel("Frequency")
+plt.ylabel("Magnitude")
+plt.title("Power spectrum")
+
+# take half of the spectrum and frequency
+left_spectrum = spectrum[:int(len(spectrum)/2)]
+left_f = f[:int(len(spectrum)/2)]
+
+# plot spectrum
+plt.figure(figsize=FIG_SIZE)
+plt.plot(left_f, left_spectrum, alpha=0.4)
+plt.xlabel("Frequency")
+plt.ylabel("Magnitude")
+plt.title("Power spectrum")
+
+# short time Fourier transform
+# STFT -> spectrogram
+hop_length = 512 # in num. of samples
+n_fft = 2048 # window in num. of samples
+
+# calculate duration hop length and window in seconds
+hop_length_duration = float(hop_length)/sample_rate
+n_fft_duration = float(n_fft)/sample_rate
+
+# perform stft
+stft = librosa.stft(signal, n_fft=n_fft, hop_length=hop_length)
+
+# calculate abs values on complex numbers to get magnitude
+spectrogram = np.abs(stft)
+
+# display spectrogram
+plt.figure(figsize=FIG_SIZE)
+librosa.display.specshow(spectrogram, sr=sample_rate, hop_length=hop_length)
+plt.xlabel("Time")
+plt.ylabel("Frequency")
+plt.colorbar()
+plt.title("Spectrogram")
+
+# apply logarithm to cast amplitude to Decibels
+# also smoothens
+log_spectrogram = librosa.amplitude_to_db(spectrogram)
+
+plt.figure(figsize=FIG_SIZE)
+librosa.display.specshow(log_spectrogram, sr=sample_rate, hop_length=hop_length)
+plt.xlabel("Time")
+plt.ylabel("Frequency")
+plt.colorbar(format="%+2.0f dB")
+plt.title("Spectrogram (dB)")
+
+
+# MFCCs
+# extract 13 MFCCs
+MFCCs = librosa.feature.mfcc(y=signal, sr=sample_rate, n_fft=n_fft, hop_length=hop_length, n_mfcc=13)
+
+# display MFCCs
+plt.figure(figsize=FIG_SIZE)
+librosa.display.specshow(MFCCs, sr=sample_rate, hop_length=hop_length)
+plt.xlabel("Time")
+plt.ylabel("MFCC coefficients")
+plt.colorbar()
+plt.title("MFCCs")
+
+# show plots
+plt.show()
